c7bc8eef1061885b70bcfdd2369ceebb9561e160
Ton Smeele <a.p.m.smeele@uu.nl>
2021-08-04 19:37:36 +0200

    docu update runonce

--- roles/runonce/files/runonce.sh
+++ roles/runonce/files/runonce.sh
@@ -4,15 +4,14 @@
 #
 # This script will execute, only once, a set of scripts for a new Linux user (upon login)
 # The target scripts (must be executable) should be placed
-# in (a subdir of) directory /etc/skel/runonce.d 
+# in (a subdir of) directory /etc/runonce.d 
 #
 # requires interactive bash/zsh shell
 if [ -h ~/runonce.d -a -d /etc/runonce.d -a -n "$PS1" ] && [ -n "$BASH_VERSION" -o -n "$ZSH_VERSION"] ]; then
-	# show date so that user can review how long configuration activity has taken
 	for i in `find -L ~/runonce.d -type f -a -executable -print|sort`
 	do
-	echo "--- Runonce: executing $i"
-	$i
+		echo "--- Runonce: executing $i"
+		$i
 	done;
 	rm ~/runonce.d
 fi
