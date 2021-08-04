240b9bab4cb257b92a9c57f0247cd9b3dadd1923
Ton Smeele <a.p.m.smeele@uu.nl>
2021-08-04 19:12:20 +0200

    runonce support all exe scripts

--- roles/runonce/files/runonce.sh
+++ roles/runonce/files/runonce.sh
@@ -2,16 +2,17 @@
 # /etc/profile.d/runonce
 # Aug2021/TSM
 #
-# This script will execute, only once, a set of bash scripts for a new Linux user (upon login)
-# The target scripts should be placed in directory /etc/skel/runonce.d 
+# This script will execute, only once, a set of scripts for a new Linux user (upon login)
+# The target scripts (must be executable) should be placed
+# in (a subdir of) directory /etc/skel/runonce.d 
 #
 # requires interactive bash/zsh shell
 if [ -h ~/runonce.d -a -d /etc/runonce.d -a -n "$PS1" ] && [ -n "$BASH_VERSION" -o -n "$ZSH_VERSION"] ]; then
 	# show date so that user can review how long configuration activity has taken
-	echo "Please stand by while your environment is configured"
-	echo "(executing scripts copied from /etc/skel/runonce.d)"
-	echo "Started configuration at: `date`"
-	source ~/runonce.d/*
-	echo "Finished configuration at: `date`"
+	for i in `find -L ~/runonce.d -type f -a -executable -print|sort`
+	do
+	echo "--- Runonce: executing $i"
+	$i
+	done;
 	rm ~/runonce.d
 fi
