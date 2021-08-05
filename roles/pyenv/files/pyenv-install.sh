03d3a4222b7e0d7f3f418ac2d1b8c1ca0ab4ee84
Ton Smeele <a.p.m.smeele@uu.nl>
2021-08-05 17:24:05 +0200

    fix line in .profile insert before

--- roles/pyenv/files/pyenv-install.sh
+++ roles/pyenv/files/pyenv-install.sh
@@ -3,15 +3,10 @@
 cd
 curl https://pyenv.run | bash
 #
-# todo? pyenv recommends below cmds to be added to .profile
-#       and be executed before ~/.bashrc is sourced
-# 
-#
-cat >>~/.profile <<'INPUTEND1'
-# added by runonce as pyenv config:
+cat /dev/stdin ~/.profile >>~/.profile <<'INPUTEND1'
+# added by runonce as pyenv config, must precede source .bashrc:
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"
-# load pyenv and pyenv-virtualenv automatically
 INPUTEND1
 cat >>~/.bashrc <<'INPUTEND2'
 # added by runonce as pyenv config
