3041fe6ef20c481c8416e8e2c8c9a718d0bbc393
Ton Smeele <a.p.m.smeele@uu.nl>
2021-08-05 14:18:13 +0200

    runonce use current instead of subshell

--- roles/runonce/files/runonce.sh
+++ roles/runonce/files/runonce.sh
@@ -6,12 +6,14 @@
 # The target scripts (must be executable) should be placed
 # in (a subdir of) directory /etc/runonce.d 
 #
+# NB: the target scripts are executed in the current shell.
+#
 # requires interactive bash/zsh shell
 if [ -h ~/runonce.d -a -d /etc/runonce.d -a -n "$PS1" ] && [ -n "$BASH_VERSION" -o -n "$ZSH_VERSION"] ]; then
 	for i in `find -L ~/runonce.d -type f -a -executable -print|sort`
 	do
 		echo "--- Runonce: executing $i"
-		$i
+		. $i
 	done;
 	rm ~/runonce.d
 fi
