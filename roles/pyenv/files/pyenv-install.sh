6545128fe0581baea9427795b90e865715bfb77c
Ton Smeele <a.p.m.smeele@uu.nl>
2021-08-05 14:11:55 +0200

    add doc to pyenv

--- roles/pyenv/files/pyenv-install.sh
+++ roles/pyenv/files/pyenv-install.sh
@@ -3,9 +3,13 @@
 cd
 curl https://pyenv.run | bash
 #
+# todo? pyenv recommends below cmds to be added to .profile
+#       and be executed before ~/.bashrc is sourced
 cat >>~/.bashrc <<'INPUTEND'
 # added by runonce as pyenv config:
-export PATH="$HOME/.pyenv/bin:$PATH"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+# load pyenv and pyenv-virtualenv automatically
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 INPUTEND
