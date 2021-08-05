b3101fbc7bf9228375bb87a6c1db0a2f4249f86d
Ton Smeele <a.p.m.smeele@uu.nl>
2021-08-05 14:27:05 +0200

    pyenv vars in login shell

--- roles/pyenv/files/pyenv-install.sh
+++ roles/pyenv/files/pyenv-install.sh
@@ -5,6 +5,8 @@ curl https://pyenv.run | bash
 #
 # todo? pyenv recommends below cmds to be added to .profile
 #       and be executed before ~/.bashrc is sourced
+# 
+#
 cat >>~/.bashrc <<'INPUTEND'
 # added by runonce as pyenv config:
 export PYENV_ROOT="$HOME/.pyenv"
@@ -13,4 +15,9 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 INPUTEND
+# add pyenv to current login shell (to support immediate use in runonce scripts)
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
 
