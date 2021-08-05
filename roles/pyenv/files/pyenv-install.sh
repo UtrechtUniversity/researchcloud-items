c1bfa1b95f5f91a1383870477963946f05aff27a
Ton Smeele <a.p.m.smeele@uu.nl>
2021-08-05 17:45:55 +0200

    fix cat

--- roles/pyenv/files/pyenv-install.sh
+++ roles/pyenv/files/pyenv-install.sh
@@ -3,11 +3,15 @@
 cd
 curl https://pyenv.run | bash
 #
-cat /dev/stdin ~/.profile >>~/.profile <<'INPUTEND1'
+cp ~/.profile ~/.profile.old
+cat /dev/stdin ~/.profile.old >~/.profile <<'INPUTEND1'
 # added by runonce as pyenv config, must precede source .bashrc:
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"
+#
 INPUTEND1
+#
+rm ~/.profile.old
 cat >>~/.bashrc <<'INPUTEND2'
 # added by runonce as pyenv config
 eval "$(pyenv init -)"
