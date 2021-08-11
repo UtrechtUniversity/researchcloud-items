df21c6cbeb66e22cd8550e2a6b18c75e5567a506
Ton Smeele <a.p.m.smeele@uu.nl>
2021-08-11 14:53:23 +0200

    runonce silently

--- roles/runonce/files/runonce.sh
+++ roles/runonce/files/runonce.sh
@@ -10,10 +10,11 @@
 #
 # requires interactive bash/zsh shell
 if [ -h ~/runonce.d -a -d /etc/runonce.d -a -n "$PS1" ] && [ -n "$BASH_VERSION" -o -n "$ZSH_VERSION"] ]; then
+        date >~/.runonce.log
 	for i in `find -L ~/runonce.d -type f -a -executable -print|sort`
 	do
-		echo "--- Runonce: executing $i"
-		. $i
+		echo "--- Runonce: executing $i" >>~/.runonce.log 2>&1
+		. $i >>~/.runonce.log 2>&1
 	done;
 	rm ~/runonce.d
 fi
