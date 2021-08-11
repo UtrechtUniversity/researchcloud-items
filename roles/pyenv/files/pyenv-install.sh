c70cc43ec5277102d4d95d656bece89914ea2815
Ton Smeele <a.p.m.smeele@uu.nl>
2021-08-11 14:51:07 +0200

    add pyenv vars to shell

--- roles/pyenv/files/pyenv-install.sh
+++ roles/pyenv/files/pyenv-install.sh
@@ -3,6 +3,7 @@
 cd
 curl https://pyenv.run | bash
 #
+# add pyenv to .profile for future logins
 cp ~/.profile ~/.profile.old
 cat /dev/stdin ~/.profile.old >~/.profile <<'INPUTEND1'
 # added by runonce as pyenv config, must precede source .bashrc:
@@ -17,4 +18,8 @@ cat >>~/.bashrc <<'INPUTEND2'
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 INPUTEND2
+#
+# also add pyenv to current login shell
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"
 
