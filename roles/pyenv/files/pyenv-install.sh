20d4d835e3832f423c78bb5cddf249a9cf17c6ac
Ton Smeele <a.p.m.smeele@uu.nl>
2021-08-12 10:29:01 +0200

    pyenv only modifies .bashrc

--- roles/pyenv/files/pyenv-install.sh
+++ roles/pyenv/files/pyenv-install.sh
@@ -4,22 +4,26 @@ cd
 curl https://pyenv.run | bash
 #
 # add pyenv to .profile for future logins
-cp ~/.profile ~/.profile.old
-cat /dev/stdin ~/.profile.old >~/.profile <<'INPUTEND1'
+#cp ~/.profile ~/.profile.old
+#cat /dev/stdin ~/.profile.old >~/.profile <<'INPUTEND1'
 # added by runonce as pyenv config, must precede source .bashrc:
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"
+#export PYENV_ROOT="$HOME/.pyenv"
+#export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"
 #
-INPUTEND1
+#INPUTEND1
 #
-rm ~/.profile.old
+#rm ~/.profile.old
 cat >>~/.bashrc <<'INPUTEND2'
 # added by runonce as pyenv config
+if [ -z "$PYENV_ROOT" ]; then
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"
+fi
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 INPUTEND2
 #
 # also add pyenv to current login shell
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"
+#export PYENV_ROOT="$HOME/.pyenv"
+#export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"
 
