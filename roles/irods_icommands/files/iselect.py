32d57d65da6fd6e3a8910acdae9b2f505f02fe3a
Ton Smeele <a.p.m.smeele@uu.nl>
2021-08-12 19:51:20 +0200

    added iselect to icommands

---
+++ roles/irods_icommands/files/iselect.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+# iselect.py
+# Aug-2021 Ton Smeele - Utrecht University
+# 
+
+from pathlib import Path
+import json
+
+# input file IRODSZONESFILE
+# format must be: 
+#   { "zone1": { "description" : "This is zone 1",
+#                "config" : { ... }
+#              },
+#     "zone2": { ... }
+#   }
+# where the "config" json object will be the content for the outputfile
+IRODSZONESFILE = '/etc/irods_zones.json'
+#
+# output file IRODSENVFILE:
+IRODSENVFILE = '~/.irods/irods_environment.json'
+
+def readFile(textfile):
+    with textfile.open(mode = 'rt') as f:
+        data = f.read()
+    f.close()
+    return data 
+
+def writeFile(textfile, data):
+    parentDir = textfile.parent
+    if not textfile.parent.is_dir():
+        # create dir(s) if nonexistent
+        textfile.parent.mkdir(parents = True)
+    with textfile.open(mode = 'wt+') as f:
+        f.write(data)
+    f.close()
+
+def selector(zones):
+    zoneChosen = ''
+    while zoneChosen not in zones:
+        print('Name: - Description:')
+        for name, props in zones.items():
+            print(name + "\t : " + props['description'])
+        if not zoneChosen == '':
+            print('\nSorry, your input was not recognized as a name in the list')
+        zoneChosen = input('\nPlease select a name from the list above: ').lower()
+    return zoneChosen
+
+
+def main():
+    inPath = Path(IRODSZONESFILE).expanduser()
+    outPath = Path(IRODSENVFILE).expanduser()
+    if not inPath.is_file():
+        print('Required inputfile ' + str(inPath) + ' not found.')
+        exit(1)
+    data = readFile(inPath)
+    zones = json.loads(data)
+    zoneChosen = selector(zones)
+    data = json.dumps(zones[zoneChosen]['config'])
+    writeFile(outPath, data)
+    print('Config written to ' + str(outPath))
+
+
+# main program
+if __name__ == "__main__":
+    main()
