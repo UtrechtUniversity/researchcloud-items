1b0933b63cb75beae235d22f231c7567e1593973
Sytse Groenwold <sytsegroenwold@gmail.com>
2022-04-19 11:10:37 +0200

    bug: typo in path

--- roles/poetry/files/poetry.sh
+++ roles/poetry/files/poetry.sh
@@ -9,6 +9,6 @@ cat >> ~/.bashrc <<'INPUTEND'
 # added by runonce as poetry config
 if [ -z "$POETRY_ROOT" ]; then
 export POETRY_ROOT="~/.local/bin"
-export PATH="$POETRY_ROOT/bin:$PATH"
+export PATH="$POETRY_ROOT:$PATH"
 fi
 INPUTEND
