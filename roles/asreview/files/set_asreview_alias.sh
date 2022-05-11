94bf679b8573fc92631a8061c9ad5dbb022c10c9
Sytse Groenwold <sytsegroenwold@gmail.com>
2022-05-11 14:48:21 +0200

    create function for asreview as command

--- roles/asreview/files/set_asreview_alias.sh
+++ roles/asreview/files/set_asreview_alias.sh
@@ -1,1 +1,7 @@
-alias asreview lab='source /opt/venvs/asreview/bin/activate && asreview lab'
+asreview() {
+    if [[ $@ == "lab" ]]; then
+        command source /opt/venvs/asreview/bin/activate && asreview lab | more
+    else
+        command asreview "$@"
+    fi
+}
