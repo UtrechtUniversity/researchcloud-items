534ddb2374f40690fa648786e6a92091bbf8f54d
Dawa Ometto <d.l.a.ometto1@uu.nl>
2023-12-20 10:47:00 +0100

    Squashed 'playbooks/roles/ext/juju4.gpgkey_generate/' content from commit af10daf
    git-subtree-dir: playbooks/roles/ext/juju4.gpgkey_generate
    git-subtree-split: af10dafa059ff977dd643dcb9d72891f2b46aabc

---
+++ test/integration/default/serverspec/gpgkey_spec.rb
@@ -0,0 +1,28 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+describe service('haveged'), :if => os[:family] == 'redhat' do
+  it { should be_enabled }
+## lxc: haveged killed with 'haveged: Fail:set_watermark()!', can't write on /proc/sys/kernel/random/write_wakeup_threshold
+##	entropy still raised/good
+#  it { should be_running }
+end
+
+describe file('/home/myuser/dupont.pub') do
+  it { should be_file }
+end
+
+describe file('/home/myuser/dupont.priv'), :if => os[:family] == 'redhat' do
+  it { should be_file }
+end
+
+describe file('/home/myuser/dupont.priv'), :if => os[:family] == 'ubuntu' && os[:release] != '18.04' do
+  it { should be_file }
+end
+
+describe file('/home/myuser/dupont.asc') do
+  it { should be_file }
+  its(:content) { should match /-----BEGIN PGP PUBLIC KEY BLOCK-----/ }
+end
