2abe59eb3dd99b8d29382c93692314f8722d2d4a
Dawa Ometto <d.l.a.ometto1@uu.nl>
2026-03-31 12:45:40 +0200

    Squashed 'molecule/ext/molecule-src/' changes from d97ad09..83ed32b
    83ed32b Update podman requirement from ~=5.7 to ~=5.8
    0ff5556 Use venv for ansible commands (#62)
    99bfea6 Test on noble instead of focal
    d0abe62 Bump ansible/ansible-lint from 26.2.0 to 26.3.0
    8aea7c8 Update molecule requirement from ~=26.2 to ~=26.3
    7d0fe13 Update molecule requirement from ~=25.12 to ~=26.2
    362bec0 Bump ansible/ansible-lint from 26.1.1 to 26.2.0
    b2b1308 Update jmespath requirement from ~=1.0 to ~=1.1
    42c86ec Update podman requirement from ~=5.6 to ~=5.7
    ca0d047 Bump ansible/ansible-lint from 26.1.0 to 26.1.1
    27d65fc Bump ansible/ansible-lint from 25.12.2 to 26.1.0
    5c67e90 Bump ansible/ansible-lint from 25.12.1 to 25.12.2
    13afb15 Bump ansible/ansible-lint from 25.12.0 to 25.12.1
    45cb615 Bump ansible/ansible-lint from 25.11.1 to 25.12.0
    bab2cc5 Update molecule requirement from ~=25.11 to ~=25.12
    03f21fd Update molecule version to 25.11
    a78fe5a Update default.env.yml
    
    git-subtree-dir: molecule/ext/molecule-src
    git-subtree-split: 83ed32b5f1ed5565e24a0da257a857aa883b778c

--- playbooks/roles/security_updates/files/start_upgrade_timers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-systemctl enable apt-daily.timer
-systemctl enable apt-daily-upgrade.timer
-systemctl start apt-daily.timer
-systemctl start apt-daily-upgrade.timer
-
-systemctl stop upgrade-bootstrap.timer
-systemctl disable upgrade-bootstrap.timer
